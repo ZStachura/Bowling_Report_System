df96224aa291ad3e4fc407623994ae513fa54cbf
ZStachura <85929734+ZStachura@users.noreply.github.com>
2024-01-26 08:15:57 +0100

    Change in pipe

--- bowling-report-system/src/shared/pipes/calculate-score/calculate-score.pipe.ts
+++ bowling-report-system/src/shared/pipes/calculate-score/calculate-score.pipe.ts
@@ -9,7 +9,9 @@ export class CalculateScorePipe implements PipeTransform {
         finalScore += (10 + scores[i+2]+scores[i+3])
       } else if(scores[i]+scores[i+1]==10){
         finalScore += (scores[i]+scores[i+1]+ scores[i+2])
-      } else {
+      } else if(scores[i+1]==='X') {
+        finalScore += (10+scores[i+2])
+      }else {
         finalScore += (scores[i]+scores[i+1])
       }
     }
